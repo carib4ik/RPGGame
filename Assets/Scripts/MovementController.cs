414d1b711954a9fff9c7eca3302cfa83101d6828
carib4ik <83468679+carib4ik@users.noreply.github.com>
2024-06-09 11:45:53 +0800

    Added getting point to move caracter

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -13,9 +13,26 @@ public class MovementController : MonoBehaviour
 
     private void Update()
     {
+        // Получаем вектор точки клика мышью
+        GetDestination();
+        
         // Перемещаем персонажа в направлении _destination.
         _navMeshAgent.SetDestination(_destination);
         
         // TODO: Получите точку, по которой кликнули мышью и задайте ее вектор в поле _destination.
+    }
+    
+    private void GetDestination()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out var hitInfo, 100f))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                var point = hitInfo.point;
+                _destination = new Vector3(point.x, point.y, point.z);
+            }
+        }
     }
 }
